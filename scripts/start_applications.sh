c2fb1130f4507f5f6cfaa693a1df270915c2b717
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2024-12-21 14:52:20 +0000

    update appspec yaml

--- scripts/start_applications.sh
+++ scripts/start_applications.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 streamlit run /root/streamlit-project/main.py --server.port 80 > /dev/null 2> /dev/null < /dev/null &
 python /root/streamlit-project/back_end/app.py > /dev/null 2> /dev/null < /dev/null &
-
-
-rm -rf /opt/codedeploy-agent/deployment-root/*
 exit 0
